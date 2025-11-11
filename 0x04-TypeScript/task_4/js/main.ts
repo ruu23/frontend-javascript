ad86cc75e3c2917400d245b8f2779fbda8079171
Arwa Mahmoud <arwamah777@gmail.com>
2025-11-11 20:55:05 +0200

    Update main.ts

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,6 +1,6 @@
-export const cpp = new Subjects.Cpp
-export const java = new Subjects.Java
-export const react = new Subjects.ReactClass
+export const cpp = new Subjects.Cpp()
+export const java = new Subjects.Java()
+export const react = new Subjects.ReactClass()
 
 export const cTeacher: Subjects.Teacher = {
     firstName: 'Arwa',
